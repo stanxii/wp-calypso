8946c7a994397e83d501eaa6965e33f5f6147c5a
AnnaMag <anna.mag.kedzierska@gmail.com>
2017-08-31 22:56:14 +0200

    Site Settings: create a Return button & use it in the Disconnect Site
    Creates a `Back` button component with Site Settings
    design in mind (wrt positioning).
    
    Adds the button to the Disconnect Site with redirection set to
    `/settings/manage-connection/:site`.

--- client/my-sites/site-settings/disconnect-site/index.jsx
+++ client/my-sites/site-settings/disconnect-site/index.jsx
@@ -12,38 +12,55 @@ import { localize } from 'i18n-calypso';
 import Card from 'components/card';
 import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
-import { getSelectedSite } from 'state/ui/selectors';
+import { getSelectedSiteSlug } from 'state/ui/selectors';
 import Main from 'components/main';
 import Placeholder from 'my-sites/site-settings/placeholder';
 import redirectNonJetpack from 'my-sites/site-settings/redirect-non-jetpack';
 import SkipSurvey from './skip-survey';
+import ReturnPreviousPage from 'my-sites/site-settings/return-previous-page';
 
 class DisconnectSite extends Component {
-	render() {
-		const { site, translate } = this.props;
+	// the flow starts at /settings/manage-connection
+	// so let this be the default redirect if no previous page is provided
 
-		if ( ! site ) {
+	getRoute() {
+		const { siteSlug } = this.props;
+
+		if ( siteSlug ) {
+			return '/settings/manage-connection/' + siteSlug;
+		}
+	}
+
+	render() {
+		const { siteSlug, translate } = this.props;
+
+		if ( ! siteSlug ) {
 			return <Placeholder />;
 		}
 		return (
-			<Main className="disconnect-site site-settings">
-				<DocumentHead title={ translate( 'Site Settings' ) } />
-				<FormattedHeader
-					headerText={ translate( 'Disconnect Site' ) }
-					subHeaderText={ translate(
-						'Tell us why you want to disconnect your site from WordPress.com.'
-					) }
-				/>
-				<Card className="disconnect-site__card"> </Card>
-				<SkipSurvey />
-			</Main>
+			<div>
+				<span className="disconnect-site__back-button">
+					<ReturnPreviousPage redirectRoute={ this.getRoute() } { ...this.props } />
+				</span>
+				<Main className="disconnect-site__site-settings">
+					<DocumentHead title={ translate( 'Site Settings' ) } />
+					<FormattedHeader
+						headerText={ translate( 'Disconnect Site' ) }
+						subHeaderText={ translate(
+							'Tell us why you want to disconnect your site from WordPress.com.'
+						) }
+					/>
+					<Card className="disconnect-site__card"> </Card>
+					<SkipSurvey />
+				</Main>
+			</div>
 		);
 	}
 }
 
 const connectComponent = connect( state => {
 	return {
-		site: getSelectedSite( state ),
+		siteSlug: getSelectedSiteSlug( state ),
 	};
 } );
 
