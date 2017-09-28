424d381540856166b5a810601aff7ebbd17c77da
Jason Moon <jason.moon@automattic.com>
2017-09-28 16:54:18 -0600

    Lib: Use Map for tracking callbacks

--- client/lib/load-script/callback-executor.js
+++ client/lib/load-script/callback-executor.js
@@ -1,3 +1,4 @@
+/** @format */
 /**
  * External dependencies
  */
@@ -7,69 +8,72 @@ const debug = debugFactory( 'lib/load-script/callback-handler' );
 /**
  * Module variables
  */
-const callbacksForURLsInProgress = {};
+const callbacksForURLsInProgress = new Map();
+
+export function getCallbackMap() {
+	return callbacksForURLsInProgress;
+}
 
 export function isLoading( url ) {
-	return callbacksForURLsInProgress.hasOwnProperty( url );
-}
-
-export function removeScriptCallback( url, callback ) {
-	debug( `Removing a known callback for a script from "${ url }"` );
-
-	if ( ! callbacksForURLsInProgress.hasOwnProperty( url ) ) {
-		return;
-	}
-
-	const index = callbacksForURLsInProgress[ url ].indexOf( callback );
-
-	if ( -1 === index ) {
-		return;
-	}
-
-	if ( 1 === callbacksForURLsInProgress[ url ].length ) {
-		delete callbacksForURLsInProgress[ url ];
-		return;
-	}
-
-	callbacksForURLsInProgress[ url ].splice( index, 1 );
-}
-
-export function removeScriptCallbacks( url ) {
-	debug( `Removing all callbacks for a script from "${ url }"` );
-
-	delete callbacksForURLsInProgress[ url ];
-}
-
-export function removeAllScriptCallbacks() {
-	debug( 'Removing all callbacks for scripts from all URLs' );
-
-	Object.keys( callbacksForURLsInProgress ).map( key => {
-		delete callbacksForURLsInProgress[ key ];
-	} );
-}
-
-export function getCallbacks() {
-	return callbacksForURLsInProgress;
+	return callbacksForURLsInProgress.has( url );
 }
 
 export function addScriptCallback( url, callback ) {
 	if ( isLoading( url ) ) {
 		debug( `Adding a callback for an existing script from "${ url }"` );
-		callbacksForURLsInProgress[ url ].push( callback );
+		callbacksForURLsInProgress.get( url ).push( callback );
 	} else {
 		debug( `Adding a callback for a new script from "${ url }"` );
-		callbacksForURLsInProgress[ url ] = [ callback ];
+		callbacksForURLsInProgress.set( url, [ callback ] );
 	}
 }
 
-export function executeCallbacks( url, callbackArguments = null ) {
-	if ( callbacksForURLsInProgress.hasOwnProperty( url ) ) {
-		const debugMessage = `Executing callbacks for "${ url }"`;
-		debug( callbackArguments === null ? debugMessage : debugMessage + ` with args "${ callbackArguments }"` );
+export function removeScriptCallback( url, callback ) {
+	debug( `Removing a known callback for a script from "${ url }"` );
 
-		callbacksForURLsInProgress[ url ].filter( cb => typeof cb === 'function' ).forEach( function( cb ) {
-			cb( callbackArguments );
-		} );
-		delete callbacksForURLsInProgress[ url ];
+	if ( ! callbacksForURLsInProgress.has( url ) ) {
+		return;
+	}
+
+	const index = callbacksForURLsInProgress.get( url ).indexOf( callback );
+
+	if ( -1 === index ) {
+		return;
+	}
+
+	if ( 1 === callbacksForURLsInProgress.get( url ).length ) {
+		delete callbacksForURLsInProgress.delete( url );
+		return;
+	}
+
+	callbacksForURLsInProgress.get( url ).splice( index, 1 );
+}
+
+export function removeScriptCallbacks( url ) {
+	debug( `Removing all callbacks for a script from "${ url }"` );
+
+	callbacksForURLsInProgress.delete( url );
+}
+
+export function removeAllScriptCallbacks() {
+	debug( 'Removing all callbacks for scripts from all URLs' );
+
+	callbacksForURLsInProgress.clear();
+}
+
+export function executeCallbacks( url, callbackArguments = null ) {
+	if ( callbacksForURLsInProgress.has( url ) ) {
+		const debugMessage = `Executing callbacks for "${ url }"`;
+		debug(
+			callbackArguments === null
+				? debugMessage
+				: debugMessage + ` with args "${ callbackArguments }"`
+		);
+
+		callbacksForURLsInProgress
+			.get( url )
+			.filter( cb => typeof cb === 'function' )
+			.forEach( cb => cb( callbackArguments ) );
+		callbacksForURLsInProgress.delete( url );
 	}
 }
